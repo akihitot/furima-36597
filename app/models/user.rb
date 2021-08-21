8584b077c030b6d5617bbe5e67bc0049d3ab52ce
akihitot <atokimatu2@gmail.com>
2021-08-22 07:01:48 +0900

    ストロングパラメータ、バリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+validates :nickname, presence: true
+validates :email, presence: true
+validates :encrypted_password, presence: true
+validates :first_name, presence: true
+validates :last_name, presence: true
+validates :first_name_kana, presence: true
+validates :last_name_kana, presence: true
+validates :birthday, presence: true
+end
