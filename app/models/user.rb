1c6334f6c70950ed7792e1c811cfae339a9fe591
akihitot <atokimatu2@gmail.com>
2021-08-22 17:05:58 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,4 @@ validates :last_name, presence: true
 validates :first_name_kana, presence: true
 validates :last_name_kana, presence: true
 validates :birthday, presence: true
-
-has_one_attached :image
 end
