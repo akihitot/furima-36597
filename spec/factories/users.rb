152ddae103fa3348f92f46a89a6c6a6d6c2509b9
akihitot <atokimatu2@gmail.com>
2021-08-23 13:15:58 +0900

    Rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 4)}
-    email                 {Faker::Internet.free_email}
-    password              {"111aaa"}
-    password_confirmation {password}
-    first_name            {"昭人"}
-    last_name             {"時松"}
-    first_name_kana       {"アキヒト"}
-    last_name_kana        {"トキマツ"}
-    birthday              {Faker::Date.birthday}
+    nickname              { Faker::Name.initials(number: 4) }
+    email                 { Faker::Internet.free_email }
+    password              { '111aaa' }
+    password_confirmation { password }
+    first_name            { '昭人' }
+    last_name             { '時松' }
+    first_name_kana       { 'アキヒト' }
+    last_name_kana        { 'トキマツ' }
+    birthday              { Faker::Date.birthday }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -117,4 +117,3 @@ RSpec.describe User, type: :model do
     end
   end
 end
-
