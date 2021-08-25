c8db17b94ad7c4a73c4620255b35e7e785b35f01
akihitot <atokimatu2@gmail.com>
2021-08-25 18:32:05 +0900

    Itemモデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name                        { Faker::Name.initials(number: 4) }
+    explanation                 { Faker::Name.initials(number: 10) }
+    category_id                 { '2' }
+    status_id                   { '2' }
+    shipping_charge_id          { '2' }
+    prefecture_id               { '2' }
+    days_to_ship_id             { '2' }
+    price                       { '300' }
     
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/google-play.png'), filename: 'google-play.png', content_type: 'image/png')
+    end
   end
 end
