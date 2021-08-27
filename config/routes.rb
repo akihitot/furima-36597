5c9c6a96a727c5d704a80218162d9554daa34345
akihitot <atokimatu2@gmail.com>
2021-08-27 11:42:04 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
-  resources :items, only:[:index,:new,:create]
+  resources :items, only:[:index,:new,:create,:show]
 end
