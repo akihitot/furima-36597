5786bd28ce16f6ebced6ae148765d7e4623fed4a
akihitot <atokimatu2@gmail.com>
2021-08-27 18:10:27 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
-  before_action :get_item_record, only: [:show,:edit,:update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update,:destroy]
+  before_action :move_to_index, only: [:edit, :update,:destroy]
+  before_action :get_item_record, only: [:show,:edit,:update,:destroy]
 
   def index
     @items = Item.includes(:user).order(id: 'DESC')
@@ -37,6 +37,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
   private
 
   def item_params
